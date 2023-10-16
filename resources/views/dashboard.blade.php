7c0b76775609e867ea63a56853fcad3a43e74dc9
nandikayp <nandikapurwanto@gmail.com>
2023-10-16 13:56:42 +0700

    ppp

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.master')
+@section('konten')
+    <h4>Selamat Datang <b>{{ Auth::user()->name }}</b>,
+        Anda Login sebagai <b>{{ Auth::user()->role }}</b>.
+    </h4>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginCon;
+use App\Http\Controllers\DashboardCon;
+use App\Http\Controllers\RegisterCon;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('components.home');
+});
+
+Route::get('/produk', function () {
+    return view('components.produk');
+});
+
+
+
+// Login
+
+Route::get('/masuk', [LoginCon::class, 'login'])->name('login');
+Route::post('actionlogin', [LoginCon::class, 'actionlogin'])->name('actionlogin');
+Route::get('dashboard', [DashboardCon::class, 'index'])->name('dashboard')->
+middleware('auth');
+Route::get('actionlogout', [LoginCon::class, 'actionlogout'])->name('actionlogout')->
+middleware('auth');
+
+
+// Register
+
+Route::get('register', [RegisterCon::class, 'register'])->name('register');
+Route::post('register/action', [RegisterCon::class, 'actionregister'])-> name('actionregister');
